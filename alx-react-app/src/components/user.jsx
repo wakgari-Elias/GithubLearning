55a6df55341f986edf7cc0530c90b4c98d6660c8
Elias wakgari <154916266+wakgari-Elias@users.noreply.github.com>
2024-09-28 14:29:42 +0300

    second commit

---
+++ alx-react-app/src/components/user.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const user = () => {
+  return (
+    <div>
+      <h1>This is example</h1>
+      <p>This is a paragraph</p>
+      
+    </div>
+  )
+}
+
+export default user
